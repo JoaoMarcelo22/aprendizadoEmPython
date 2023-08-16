05566d7fadfaf95bfb4b66276ba9377a18d9bb6d
JoaoMarcelo22 <joao_marcelo.22@hotmail.com>
2023-08-15 21:55:23 -0300

    feat: finalizando metodos de string

--- PrimeirosPassos/aula5.py
+++ PrimeirosPassos/aula5.py
@@ -1,1 +1,16 @@
 # Manipulando string
+
+# Metodos
+
+curso = "pYtHon"
+curso2 = "  Python "
+curso3 = "Python"
+
+print(curso.upper()) # Maiusculo
+print(curso.lower()) # Minusculo 
+print(curso.title()) # Titulo
+print(curso2.strip()) # Remove os espaços da direita e esquerda
+print(curso2.lstrip()) # Remove apenas os espaços da esquerda 
+print(curso2.rstrip()) # Remove os espaços apenas da direita
+print(curso3.center(10,"#")) # Cria uma nova string e adiciona um caracter no inicio e fim
+print(".".join(curso3)) # Cria um nova string separado por um caractere
